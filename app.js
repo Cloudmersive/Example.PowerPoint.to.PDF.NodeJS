0e81446b8a92403d4e63a3d376f61500daf1724a
Cloudmersive <35204726+Cloudmersive@users.noreply.github.com>
2019-11-25 22:06:19 -0800

    Update app.js

--- app.js
+++ app.js
@@ -31,3 +31,28 @@ var callback = function(error, data, response) {
   }
 };
 apiInstance.convertDocumentPptxToPdf(inputFile, callback);
+
+// Optionally, we can rasterize the PDF
+
+apiInstance = new CloudmersiveConvertApiClient.EditPdfApi();
+
+inputFile = Buffer.from(fs.readFileSync("C:\\temp\\output.pdf").buffer);
+
+var callback2 = function(error, data, response) {
+  if (error) {
+    console.error(error);
+  } else {
+
+    fs.writeFile("C:\\temp\\output2.pdf", data,  "binary",function(err) {
+        if(err) {
+            console.log(err);
+        } else {
+            console.log("The file was saved!");
+        }
+    });
+
+    console.log('Successful - done.');
+  }
+};
+
+apiInstance.editPdfRasterize(inputFile, callback2);
